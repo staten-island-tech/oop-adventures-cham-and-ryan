76a681e529d108285444944ef0fdab030e286410
Cham-Pan <144945585+Cham-Pan@users.noreply.github.com>
2023-11-30 13:24:40 -0500

    Update main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from teacher import Teacher
+
+Zerega = Teacher("Lauren Zerega", "AP World History")
+Whalen = Teacher("Michael Whalen", "Computer Science")
+
+Zerega.information()
+Whalen.information()
