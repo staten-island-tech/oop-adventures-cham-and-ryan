80fa87cb2d7e9a217455446d669ec3fa8b8a7456
Cham-Pan <144945585+Cham-Pan@users.noreply.github.com>
2023-11-30 13:59:17 -0500

    Delete teacher.py

--- teacher.py
+++
@@ -1,6 +0,0 @@
-class Teacher():
-    def __init__(self, name, subject):
-        self.name = name
-        self.subject = subject
-    def information(self):
-        print(f"{self.name}, {self.subject}")
