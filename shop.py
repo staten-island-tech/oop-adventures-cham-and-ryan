b0f182c5966a2f983aa50bd6f0f6d54d5318e60d
Cham-Pan <144945585+Cham-Pan@users.noreply.github.com>
2023-12-04 13:30:15 -0500

    Update shop.py

--- shop.py
+++ shop.py
@@ -2,4 +2,7 @@ class Shop():
     def __init__(self, name, item):
         self.name = name
         self.item = item
-    def sell():
+    def sell(self, sold):
+        self.products.remove(sold)
+        print(f"You have purchased {sold}")
+        print(self.item)
