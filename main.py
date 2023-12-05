6dc94ff71f7147597da8b4613e5dd8b40088b397
Cham-Pan <144945585+Cham-Pan@users.noreply.github.com>
2023-12-05 14:01:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from shop import Shop
 
-shopkeeper = Shop("Jarvis", ["Metal Detector", "Shovel", "Dog", "Map to find Jarvis' toenail"])
-shopkeeper.sell("Dog")
+player = 0
+
+item_Map = 100
+item_Shovel = 500
+item_Pickaxe = 500
+
+shopkeeper1 = Shop("Jarvis", ["Map: 100 Gold Coins", "Shovel: 500 Gold Coins", "Pickaxe: 500 Gold Coins"])
+shopkeeper1.sell("Shovel") 
+player -= item_Shovel
+print(player)
