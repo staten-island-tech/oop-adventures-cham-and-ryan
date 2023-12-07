945e34093a12a279d0dbff17a253c69f79e553ce
Cham-Pan <144945585+Cham-Pan@users.noreply.github.com>
2023-12-07 13:33:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from shop import Shop
 player = 0
 
 item_Map = 100
-item_Shovel = 500
-item_Pickaxe = 500
+item_Shovel = 250
+item_Pickaxe = 250
 
-shopkeeper1 = Shop("Jarvis", ["Map: 100 Gold Coins", "Shovel: 500 Gold Coins", "Pickaxe: 500 Gold Coins"])
-shopkeeper1.sell("Shovel") 
-player -= item_Shovel
-print(player)
+shopkeeper1 = Shop("Jarvis", ["Map", "Shovel", "Pickaxe"])
+print(f"price: Map: {item_Map} Gold Coins, Shovel: {item_Shovel}, Gold Coins Pickaxe: {item_Pickaxe} Gold Coins")
+shopkeeper1.sell("Shovel")
+
+player =- item_Shovel
+
+print(f"You have {player} gold coins.")
