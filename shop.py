8daa6afc0c8c7a8c13297511d18d9967c0090038
Cham-Pan <144945585+Cham-Pan@users.noreply.github.com>
2023-12-08 14:02:23 -0500

    Delete shop.py

--- shop.py
+++
@@ -1,7 +0,0 @@
-class Shop():
-    def __init__(self, name, item):
-        self.name = name
-        self.item = item
-    def sell(self, sold_item):
-        self.item.remove(sold_item)
-        print(f"You have purchased {sold_item}")
