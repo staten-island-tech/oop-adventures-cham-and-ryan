28e8902bf11840198cb585b4ced2386b8e78bff5
Cham-Pan <144945585+Cham-Pan@users.noreply.github.com>
2023-12-08 13:25:53 -0500

    Update shop.py

--- shop.py
+++ shop.py
@@ -3,7 +3,5 @@ class Shop():
         self.name = name
         self.item = item
     def sell(self, sold_item):
-        print(self.item)
-        print("What would you like to buy?")
         self.item.remove(sold_item)
         print(f"You have purchased {sold_item}")
