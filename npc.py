ad1ddf6d49bf25dfc03c4858585e495b8ac76bf8
Cham-Pan <144945585+Cham-Pan@users.noreply.github.com>
2023-12-08 14:02:31 -0500

    Create npc.py

---
+++ npc.py
@@ -0,0 +1,10 @@
+class Trader():
+    def __init__(self, name, item):
+        self.name = name
+        self.item = item
+    def buy_trader(self, buy_item):
+        self.item.remove(buy_item)
+        print(f"You have bought {buy_item}.")
+    def sell_trader(self, sell_item):
+        self.item.remove(sell_item)
+        print(f"You have sold {sell_item}.")
