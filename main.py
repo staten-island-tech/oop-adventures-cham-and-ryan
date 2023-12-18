5273b7b372c93941c81d16dd84aa0b990467eaec
Cham-Pan <144945585+Cham-Pan@users.noreply.github.com>
2023-12-18 13:26:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from npc import Trader
 
+import json
+x = open("data.json", encoding="utf8")
+data = json.load(x)
+
 player_gold_coins = 0
 
 item_Map = 750
@@ -10,16 +14,17 @@ item_Diamond = 150
 item_Iron = 50
 
 shopkeeper1 = Trader("Jarvis", ["Map", "Shovel", "Pickaxe"])
-print(f"price: Map: {item_Map} Gold Coins, Shovel: {item_Shovel} Gold Coins, Pickaxe: {item_Pickaxe} Gold Coins")
+print(data[1]["item"])
 shopkeeper1.buy_trader("Shovel")
-
-player1 = Trader("Eyad", ["Diamond", "Iron"])
-print(f"price: Diamond: {item_Diamond} Gold Coins, Iron: {item_Iron} Gold Coins.")
-player1.sell_trader("Diamond")
 
 player_gold_coins -= item_Shovel
 print(f"You have {player_gold_coins} gold coins.")
 
-player_gold_coins += item_Diamond
-print(f"You have {player_gold_coins} gold coins.")
+
+
+#player1 = Trader("Eyad", ["Diamond", "Iron"])
+#print(f"price: Diamond: {item_Diamond} Gold Coins, Iron: {item_Iron} Gold Coins.")
+#player1.sell_trader("Diamond")
+#player_gold_coins += item_Diamond
+#print(f"You have {player_gold_coins} gold coins.")
 
