bac2ba50bc5a56a3cf327dda94bda2704808e94b
Cham-Pan <144945585+Cham-Pan@users.noreply.github.com>
2023-12-18 14:01:48 -0500

    Update npc.py

--- npc.py
+++ npc.py
@@ -4,7 +4,7 @@ class Trader():
         self.item = item
     def buy_trader(self, buy_item):
         self.item.remove(buy_item)
-        print(f"You have bought {buy_item}.")
+        print(f"You have bought a {buy_item} from {self.name}.")
     def sell_trader(self, sell_item):
         self.item.remove(sell_item)
-        print(f"You have sold {sell_item}.")
+        print(f"You have sold a {sell_item} to {self.name}.")
