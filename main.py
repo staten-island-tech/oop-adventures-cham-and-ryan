97a5909e5a91280c7ecd29c44647d60cb72eb729
Cham-Pan <144945585+Cham-Pan@users.noreply.github.com>
2024-01-03 14:00:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,22 @@ import json
 x = open("data.json", encoding="utf8")
 data = json.load(x)
 
-def display():
-    for i in range(12):
-        print(f"{i}. {data[i]['item']}")
-        i += 1
+player_gold_coins = 10000000
 
-question_display = input("Do you want to see all buyable items?")
-if question_display == "yes":
-    display()
+def display():
+    question_display = input("Do you want to see all buyable items?")
+    if question_display == "yes":
+        for i in range(12):
+            print(f"{i}. {data[i]['item']}")
+            i += 1
+
+display()
 
 def Jarvis():
-    player_gold_coins = 45328985435893
     merchant_Jarvis = Trader("Jarvis", [data[0]["item"], data[3]["item"], data[6]["item"], data[7]["item"], data[8]["item"], data[10]["item"]])
-
+    
+    global player_gold_coins
+    
     question_buy_Jarvis = int(input("What item number would you like to buy from Jarvis the merchant?"))
     if data[question_buy_Jarvis]["price"] <= player_gold_coins:
         merchant_Jarvis.buy_trader(data[question_buy_Jarvis]["item"])
@@ -25,10 +28,25 @@ def Jarvis():
     else:
         print(f"You do not have enough gold coins to buy {data[question_buy_Jarvis]['item']}.")
 
-question_interact_npc = input("Who you like to interact with?").lower()
+
+
+def Brian():
+    merchant_Brian = Trader("Brian", [data[1]["item"], data[2]["item"], data[4]["item"], data[5]["item"], data[9]["item"], data[11]["item"]])
+    
+    global player_gold_coins
+    
+    question_buy_Brian = int(input("What item number would you like to buy from Jarvis the merchant?"))
+    if data[question_buy_Brian]["price"] <= player_gold_coins:
+        merchant_Brian.buy_trader(data[question_buy_Brian]["item"])
+        player_gold_coins -= data[question_buy_Brian]["price"]
+        print(f"You have {player_gold_coins} gold coins left.")
+    else:
+        print(f"You do not have enough gold coins to buy {data[question_buy_Brian]['item']}.")
+
+
+
+question_interact_npc = input("Who you like to interact with?")
 if question_interact_npc == "Jarvis":
     Jarvis()
-
-def Jarvis_brother():
-    player_gold_coins = 0
-    merchant_Jarvis_brother = Trader("Jarvis' brother", [data[1]["item"], data[2]["item"], data[4]["item"], data[5]["item"], data[9]["item"], data[11]["item"]])
+elif question_interact_npc == "Brian":
+    Brian()
