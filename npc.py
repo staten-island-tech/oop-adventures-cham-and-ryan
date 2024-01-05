7e2eab0f60d2ff9db15d2780f67fa970f8db8468
Cham-Pan <144945585+Cham-Pan@users.noreply.github.com>
2024-01-05 13:26:15 -0500

    Update npc.py

--- npc.py
+++ npc.py
@@ -1,3 +1,10 @@
+class Player():
+    def __init__(self, name, item):
+        self.name = name
+        self.item = item
+    def add(self, add_item):
+        self.item.append(add_item)
+
 class Trader():
     def __init__(self, name, item):
         self.name = name
@@ -5,5 +12,3 @@ class Trader():
     def buy_trader(self, buy_item):
         self.item.remove(buy_item)
         print(f"You have bought a {buy_item} from {self.name}.")
-    def display(self):
-        print(self.item)
