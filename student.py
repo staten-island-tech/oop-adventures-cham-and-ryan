14f81443c47176f1729be72452cc8b1e1e4fba37
ryans439 <144480277+ryans439@users.noreply.github.com>
2024-01-11 13:50:07 -0500

    g

--- student.py
+++
@@ -1,8 +0,0 @@
-class Student():
-    def __init__(self, name, grades, hos):
-        self.name = name
-        self.grades = grades
-        self.hos = hos
-    def upgrade(self):
-        self.grades += 5
-        return self
