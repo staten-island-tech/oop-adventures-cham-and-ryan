e51404f16e0db8a07da5697d0c61858189d4000c
Cham-Pan <144945585+Cham-Pan@users.noreply.github.com>
2024-01-11 13:27:23 -0500

    Create location.py

---
+++ location.py
@@ -0,0 +1,6 @@
+class Location():
+    def __init__(self, place, item):
+        self.place = place
+        self.item = item
+    def information(self):
+        print(f"{self.place}, {self.item}")
